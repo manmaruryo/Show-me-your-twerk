4dac542552ce972c90ed20985b161fce5cc064c7
manmaruryo <manmaruryo1173@gmail.com>
2024-11-28 03:09:31 +0900

    Update script.js

--- script.js
+++ script.js
@@ -6,10 +6,12 @@ const mainImage = document.getElementById('mainImage');
 showImageButton.addEventListener('click', function (event) {
     event.preventDefault(); // デフォルトのリンク動作を無効化
 
-    // 画像の表示/非表示を切り替え
+    // 画像の表示状態を切り替える
     if (mainImage.style.display === 'none' || mainImage.style.display === '') {
         mainImage.style.display = 'block'; // 画像を表示
+        showImageButton.style.display = 'none'; // もっと見るボタンを非表示
     } else {
         mainImage.style.display = 'none'; // 画像を非表示
+        showImageButton.style.display = 'block'; // もっと見るボタンを再表示
     }
 });
